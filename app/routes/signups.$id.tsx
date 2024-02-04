5b6504b02ec24bfe8eaed04ef399b94f5d7af884
Tyler Olson <tydotg@gmail.com>
2024-02-03 18:11:06 -0800

    signup styling

--- app/routes/signups.$id.tsx
+++ app/routes/signups.$id.tsx
@@ -1,12 +1,25 @@
 import { useLoaderData } from "@remix-run/react";
 import { LoaderFunctionArgs, TypedResponse, json } from "@remix-run/node";
-import { Signup } from "@prisma/client";
+import { Participant, Signup, SignupOption, User } from "@prisma/client";
 
 import { prisma } from "~/services/db";
 
-export async function loader({ params }: LoaderFunctionArgs): Promise<TypedResponse<Signup>> {
+type SignupWithRefs = Signup & { 
+  author: User,
+  signupOptions: (
+    SignupOption & { participants: Participant[] }
+  )[] 
+};
+
+export async function loader({ params }: LoaderFunctionArgs): Promise<TypedResponse<SignupWithRefs>> {
   const signup = await prisma.signup.findUnique({
     where: { id: String(params.id) },
+    include: {
+      author: true,
+      signupOptions: { 
+        include: { participants: true } 
+      } 
+    }
   });
 
   if (!signup) throw new Response("", { status: 404 });
@@ -18,10 +31,30 @@ export default function DashboardUser() {
   const signup = useLoaderData<typeof loader>();
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold">View signup</h1>
-      <h1 className="text-3xl font-bold">{signup.title}</h1>
-      <p className="mt-4 text-gray-500">{signup.description}</p>
+    <div className="m-10 max-w-2xl mx-auto gap-10 flex flex-col">
+      <div className="gap-4 flex flex-col">
+        <h1 className="text-6xl font-bold">{signup.title}</h1>
+        <p className="text-lg text-gray-600">{signup.description}</p>
+        <p className="text-sm">Created by {signup.author.firstName} {signup.author.lastName}</p>
+      </div>
+      <h2 className="text-2xl font-bold">Signup Options</h2>
+      <div className="gap-4 flex flex-col">
+        {signup.signupOptions.map(option => (
+          <div key={option.id} className="card border shadow">
+            <div className="card-body flex flex-row">
+              <h2 className="card-title flex-grow">{option.title}</h2>
+              <p>{new Date(option.date).toLocaleDateString()}</p>
+              <p>{option.quantity - option.participants.length} of {option.quantity} available</p>
+              <button className="btn btn-primary btn-outline">Sign Up</button>
+            </div>
+          </div>
+          // <li key={option.id}>
+          //   <h3 className="text-xl font-bold">{option.title}</h3>
+          //   <p>{option.description}</p>
+          //   <p>Participants: {option.participants.length}</p>
+          // </li>
+        ))}
+      </div>
     </div>
   )
 }
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -15,8 +15,8 @@ const main = async (): Promise<void> => {
 
   const [signup1, signup2] = await Promise.all([{
       published: true,
-      title: 'Test Signup',
-      description: 'This is a test signup',
+      title: 'Summer Camp 2024',
+      description: 'Signups for summer camp 2024. This is going to be a great time. We will have a lot of fun.',
       authorId: user.id,
     }, {
       published: true,
@@ -34,16 +34,22 @@ const main = async (): Promise<void> => {
   );
 
   const [option1, option2, _option3, _option4] = await Promise.all([{
-      title: 'Camp Week 1',
-      description: 'This is the first option',
+      title: 'Camp Week 1 - this is the best one',
+      description: 'Really fun week of camp. Everyone should sign up. Once you do make sure to bring a sleeping bag and a pillow. Also, bring a toothbrush.',
       quantity: 10,
       date: new Date('2-1-2024'),
       signupId: signup1.id,
     }, {
       title: 'Camp Week 2',
-      description: 'This is the second option',
+      description: 'This is the second week of camp. Make sure to do stuff!',
       quantity: 10,
-      date: new Date('2-2-2024'),
+      date: new Date('2-8-2024'),
+      signupId: signup1.id,
+    }, {
+      title: 'Camp Week 3',
+      description: 'This is the third week of camp. Make sure to do stuff!',
+      quantity: 10,
+      date: new Date('2-15-2024'),
       signupId: signup1.id,
     }, {
       title: 'Option 1',
