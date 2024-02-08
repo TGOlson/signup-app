dbf59c8ad4b981107bf1df831c8ae12aa5004f4e
Tyler Olson <tydotg@gmail.com>
2024-02-07 22:53:21 -0800

    hack around on editable fields

--- app/routes/signup.$id.tsx
+++ app/routes/signup.$id.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet, useLoaderData, useRouteLoaderData } from "@remix-run/react";
+import { Outlet, UIMatch, useLoaderData, useMatches, useRouteLoaderData } from "@remix-run/react";
 import { LoaderFunctionArgs, TypedResponse, json } from "@remix-run/node";
 import { Participant, Signup, SignupOption as Option, User } from "@prisma/client";
 
@@ -8,6 +8,7 @@ import NavBar from "~/components/NavBar";
 
 import { loader as rootLoader } from "../root";
 import { EnvelopeIcon } from "@heroicons/react/24/outline";
+import { handle } from "./signup.$id.edit";
 
 type SignupWithRefs = Signup & { 
   author: User,
@@ -40,16 +41,26 @@ export async function loader({ params }: LoaderFunctionArgs): Promise<TypedRespo
 export default function SignupDetails() {
   const user = useRouteLoaderData<typeof rootLoader>("root");
   const signup = useLoaderData<typeof loader>();
+  const matches = useMatches() as UIMatch<unknown, typeof handle>[];
+
+  const editable = matches.some(match => match.handle?.editable);
 
   return (
     <div className="flex flex-col min-h-screen">
       <NavBar />
       <div className="bg-diagonal flex-grow">
         <div className="max-w-3xl mx-auto flex flex-col mb-10">
+          <Outlet />
           <div className="card card-compact my-5">
             <div className="card-body">
-              <h1 className="text-6xl font-bold">{signup.title}</h1>
-              <p className="text-lg text-gray-600">{signup.description}</p>
+              {editable
+                ? <input required type="text" defaultValue={signup.title} className="input input-bordered input-lg h-20 text-6xl font-bold" />
+                : <h1 className="text-6xl font-bold">{signup.title}</h1>
+              }
+              {editable
+                ? <textarea required defaultValue={signup.description} rows={3} className="textarea textarea-bordered textarea-lg text-lg text-gray-600" />
+                : <p className="text-lg text-gray-600">{signup.description}</p>
+              }
               <p className="text-sm flex items-center gap-1">
                 Created by {signup.author.firstName} {signup.author.lastName}
                 <a href={`mailto:${signup.author.email}?subject=${signup.title}`} className="link"><EnvelopeIcon className="h-4 w-4" /></a>
@@ -57,9 +68,8 @@ export default function SignupDetails() {
             </div>
           </div>
           <div className="gap-6 flex flex-col p-0">
-            {signup.signupOptions.map(option => <SignupOption key={option.id} option={option} user={user}/>)}
+            {signup.signupOptions.map(option => <SignupOption key={option.id} option={option} user={user} editable={editable}/>)}
           </div>
-          <Outlet />
         </div>
       </div>
     </div>
